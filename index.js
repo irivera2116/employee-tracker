7a954f52a447998d0336c0f6f6303e7358b30330
irivera2116 <irivera2116@GMAIL.COM>
2023-06-26 12:16:14 -0400

    updated index and server

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .createPromptModule([
+        {
+            type: 'list',
+            message: 'What do you need?',
+            choices: ['View Employees', 'View all Roles', 'View all Departments', 'Add Employee', 'Add Role', 'Add Departmnet', 'Update Role', 'Quit'],
+        },
+    ]);
+    

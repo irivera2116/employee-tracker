78efaa5cc17751db44e3c53af019b493bea597aa
irivera2116 <irivera2116@GMAIL.COM>
2023-06-27 14:51:45 -0400

    cleaned up schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,25 @@ CREATE DATABASE business_db;
 USE business_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(300)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
-    department_id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_id INT,
     title VARCHAR(30),
     salary DECIMAL NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    CONSTRAINT fk_department,
+    FOREIGN KEY (department_id), 
+    REFERENCES department(id),
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    roles_id INTEGER,
-    manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    roles_id INT,
+    manager_id INT NOT NULL
 );
